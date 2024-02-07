07a45719b816351b3823a3a8c95d99bcc389a668
Sezgin <sezginaliunal@hotmail.com>
2024-02-08 00:38:51 +0300

    All Changes App

---
+++ lib/app/bindings/network_binding.dart
@@ -0,0 +1,11 @@
+import 'package:resume/app/controllers/network_controller.dart';
+
+import 'package:get/get.dart';
+
+class NetworkBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<NetworkController>(() => NetworkController());
+    // Get.put<HomeController>(HomeController());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,48 @@
+import 'app/bindings/network_binding.dart';
+import 'app/res/constants/strings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'app/data/services/dependency_injection.dart';
+import 'app/data/services/theme_service.dart';
+import 'app/data/services/translations_service.dart';
+import 'app/routes/app_pages.dart';
+import 'app/routes/app_routes.dart';
+import 'app/res/layouts/main/main_layout.dart';
+import 'app/res/theme/themes.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DependecyInjection.init();
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+    return ScreenUtilInit(
+      builder: (_, __) {
+        return GetMaterialApp(
+          title: AppStrings.appName,
+          debugShowCheckedModeBanner: false,
+          theme: Themes().lightTheme,
+          darkTheme: Themes().darkTheme,
+          themeMode: ThemeService.instance.themeMode,
+          translations: Translation(),
+          locale: const Locale('en'),
+          fallbackLocale: const Locale('en'),
+          initialRoute: AppRoutes.HOME,
+          initialBinding: NetworkBinding(),
+          unknownRoute: AppPages.unknownRoutePage,
+          getPages: AppPages.pages,
+          builder: (_, child) {
+            return MainLayout(child: child!);
+          },
+        );
+      },
     );
   }
 }
